a6af44c84e04416e7b8d73310d2431c9134f913f
raesyarsalan-ai <raesy.arsalan@gmail.com>
2026-01-04 09:43:16 +0330

    Update executor.py

--- app/execution/executor.py
+++ app/execution/executor.py
@@ -1,1 +1,5 @@
-
+def execute(exchange, side, symbol, amount):
+    if side == "buy":
+        exchange.create_market_buy_order(symbol, amount)
+    if side == "sell":
+        exchange.create_market_sell_order(symbol, amount)
